02ad3007c4f2f143b37fa5cb4aeca5009b80dcc1
Rafał Szóstak <szeryfowski@gmail.com>
2026-01-22 21:41:33 +0100

    SafeGive function

--- src/main/java/me/lemurxd/skyblockplugin/utils/SafeGive.java
+++ src/main/java/me/lemurxd/skyblockplugin/utils/SafeGive.java
@@ -1,12 +1,17 @@
 package me.lemurxd.skyblockplugin.utils;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+
+import java.util.Iterator;
 
 public class SafeGive {
 
     public SafeGive(ItemStack item, Player p) {
-
+        Iterator<ItemStack> iter = p.getInventory().addItem(item).values().iterator();
+        while (iter.hasNext()) {
+            p.getWorld().dropItemNaturally(p.getLocation(), iter.next());
+        }
     }
-
 }
